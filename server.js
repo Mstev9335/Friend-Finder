0ce6fc5bd1a8c32a02b72c4393ae991f16fade8a
unknown <mstevens9335@yahoo.com>
2019-04-29 17:28:45 -0400

    html routes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var path = require("path");
 var app = express();
 // var PORT = 3000;
 // need to set the port for this in order to deploy it to heroku
-// the number is the port number to be used still
+
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
