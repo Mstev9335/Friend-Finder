39f9e2f991c898a95896d89866c8f78485af2ebf
unknown <mstevens9335@yahoo.com>
2019-04-29 17:41:46 -0400

    api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,12 @@
 // API Routes
 // =============================
 app.get("/api/friends", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
+    // res.sendFile(path.join(__dirname, "home.html"));
     console.log(__dirname);
   });
 
   app.get("/api/friends", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
+    // res.sendFile(path.join(__dirname, "survey.html"));
     console.log(__dirname);
   });
   
